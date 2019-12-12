1c02196e74d7e15859ab4f23712bb9b4f87a8f11
Gulfam Ismail <gulfamismail09@gmail.com>
2019-12-12 15:33:17 +0500

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
 
 before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 before_action :correct_user,
@@ -12,6 +13,12 @@ User.find(params[:id]).destroy
 flash[:success] = "User deleted"
 redirect_to users_url
 end
+
+
+
+
+
+
 
   def new
   	@user = User.new
@@ -54,7 +61,43 @@ def index
 	@users = User.paginate(page: params[:page])
 end
 
-private
+
+
+
+
+
+
+  def following
+
+
+    @title  = "Following"
+
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+  private
 def user_params
 params.require(:user).permit(:name, :email, :password,
 :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,22 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
 
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+
 
   resources :account_activations, only: [:edit]
   resources :password_resets,
 only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :microposts,
+            only: [:create, :destroy]
+
+  resources :relationships,
             only: [:create, :destroy]
 
 end
